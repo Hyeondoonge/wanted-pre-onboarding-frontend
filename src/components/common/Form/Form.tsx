06bc43a413e21db6fb64cb5d22be57eef64297c7
Hyeondoonge <migu554@naver.com>
2023-06-15 12:14:44 +0900

    :sparkles: 유효성 상태관리 - #3

--- src/components/common/Form/Form.tsx
+++ src/components/common/Form/Form.tsx
@@ -23,39 +23,49 @@ interface ButtonProps {
   disabled: boolean;
 }
 
-function EmailInput() {
+function EmailInput({ setIsValid }: EmailInputProps) {
   const onInput = (event: FormEvent<HTMLInputElement>) => {
     const { value } = event.target as HTMLInputElement;
     const reg = new RegExp('@');
     const res = reg.test(value);
+
+    setIsValid((isValid) => ({ ...isValid, email: res }));
   };
 
   return <Styled.EmailInput data-testid='email-input' onInput={onInput} />;
 }
 
-function PasswordInput() {
+function PasswordInput({ setIsValid }: PasswordInputProps) {
   const onInput = (event: FormEvent<HTMLInputElement>) => {
     const { value } = event.target as HTMLInputElement;
     const reg = new RegExp('^.{8,}$');
     const res = reg.test(value);
+
+    setIsValid((isValid) => ({ ...isValid, password: res }));
   };
 
   return <Styled.PasswordInput data-testid='password-input' onInput={onInput} />;
 }
 
-function Button() {
-  return <Styled.SubmitButton data-testid='signup-button'>제출</Styled.SubmitButton>;
+function Button({ disabled }: ButtonProps) {
+  return (
+    <Styled.SubmitButton data-testid='signup-button' disabled={disabled}>
+      제출
+    </Styled.SubmitButton>
+  );
 }
 
 export default function Form() {
+  const [isValid, setIsValid] = useState({ email: false, password: false });
+
   return (
     <Styled.Form>
       <div>
-        <EmailInput />
-        <PasswordInput />
+        <EmailInput setIsValid={setIsValid} />
+        <PasswordInput setIsValid={setIsValid} />
       </div>
       <div>
-        <Button />
+        <Button disabled={!isValid.email || !isValid.password} />
       </div>
     </Styled.Form>
   );
