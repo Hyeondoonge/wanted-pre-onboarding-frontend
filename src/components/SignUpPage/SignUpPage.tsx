6fdb4cb4907eedac60770904b0a58fbdb398affb
Hyeondoonge <migu554@naver.com>
2023-06-15 14:17:29 +0900

    :sparkles: submit 처리 성공시 로그인 페이지로 라우팅 - #3

--- src/components/SignUpPage/SignUpPage.tsx
+++ src/components/SignUpPage/SignUpPage.tsx
@@ -1,10 +1,17 @@
 import Form from 'components/common/Form';
 import * as Styled from './SignUpPage.styled';
+import { useNavigate } from 'react-router';
+import { SIGNIN_URL } from 'constants/route';
 
 export default function SignUpPage() {
+  const navigate = useNavigate();
+  const submitHandler = () => {
+    navigate(SIGNIN_URL);
+  };
+
   return (
     <Styled.SignUpPage>
-      <Form action={'회원가입'} />
+      <Form action={'회원가입'} submitHandler={submitHandler} />
     </Styled.SignUpPage>
   );
 }
