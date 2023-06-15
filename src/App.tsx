c0bb7041583f6ed6cc9a29fd94403062d6c79fec
Hyeondoonge <migu554@naver.com>
2023-06-15 12:35:30 +0900

    :recycle: signup route 경로 상수화 - #3

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SignUpPage from 'components/SignUpPage/SignUpPage';
+import { SIGNUP_URL } from 'constants/route';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/signup" element={<SignUpPage />} />
+        <Route path={SIGNUP_URL} element={<SignUpPage />} />
       </Routes>
     </BrowserRouter>
   );
