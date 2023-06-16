a8d432d34d868635869f387db033d65255b3b1ce
Hyeondoonge <migu554@naver.com>
2023-06-16 12:23:33 +0900

    :sparkles: 회원가입 요청 상호작용 추가 - #3

--- src/components/SignUpPage/SignUpPage.tsx
+++ src/components/SignUpPage/SignUpPage.tsx
@@ -3,13 +3,36 @@ import { useState } from 'react';
 import EmailInput from 'components/common/EmailInput/EmailInput';
 import PasswordInput from 'components/common/PasswordInput/PasswordInput';
 import SubmitButton from 'components/common/SubmitButton/SubmitButton';
+import { signUp } from 'apis/auth';
+import { useNavigate } from 'react-router-dom';
+import { SIGNIN_URL } from 'constants/route';
+
+interface AuthFormEventTarget extends EventTarget {
+  email: HTMLInputElement;
+  password: HTMLInputElement;
+}
 
 export default function SignUpPage() {
   const [isValid, setIsValid] = useState({ email: false, password: false });
+  const naviagte = useNavigate();
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const $input = event.target as AuthFormEventTarget;
+    const email = $input.email.value;
+    const password = $input.password.value;
+
+    const res = await signUp({ email, password });
+    if ('message' in res) {
+      console.log(res.message);
+    } else {
+      naviagte(SIGNIN_URL);
+    }
+  };
 
   return (
     <Styled.SignUpPage>
-      <Styled.Form>
+      <Styled.Form onSubmit={handleSubmit}>
         <div>
           <EmailInput setIsValid={setIsValid} />
           <PasswordInput setIsValid={setIsValid} />
