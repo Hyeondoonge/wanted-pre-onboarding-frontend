f6995207acb9d4014e17774357c455f4f062a989
Hyeondoonge <migu554@naver.com>
2023-06-17 16:42:36 +0900

    :sparkles: Todo 추가 기능 - #4

--- src/components/TodoPage/TodoPage.tsx
+++ src/components/TodoPage/TodoPage.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react';
+import { FormEvent, useEffect, useState } from 'react';
 import * as Styled from './TodoPage.styled';
-import { getTodos } from 'apis/todo';
+import { createTodo, getTodos } from 'apis/todo';
 import { Todo } from 'interface/common';
 
 function TodoItem({ todo }: { todo: Todo }) {
@@ -32,6 +32,10 @@ function TodoItem({ todo }: { todo: Todo }) {
   );
 }
 
+interface TodoFormEventTarget extends EventTarget {
+  todo: HTMLInputElement;
+}
+
 export default function TodoPage() {
   const [todolist, setTodoList] = useState<Todo[]>([]);
 
@@ -50,11 +54,29 @@ export default function TodoPage() {
     fetchTodo();
   }, []);
 
+  const handleAddTodo = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const access_token = localStorage.getItem('access_token') || '';
+
+    const $input = event.target as TodoFormEventTarget;
+    const todo = $input.todo.value;
+
+    const result = await createTodo({ access_token, createTodoBody: { todo } });
+
+    if ('message' in result) {
+      console.log(result);
+    } else {
+      setTodoList((todo) => [...todo, result]);
+    }
+  };
+
   return (
     <Styled.TodoPage>
-      <Styled.Form>
-        <input data-testid='new-todo-input' />
-        <button data-testid='new-todo-add-button'>추가</button>
+      <Styled.Form onSubmit={handleAddTodo}>
+        <input data-testid='new-todo-input' name='todo' />
+        <button data-testid='new-todo-add-button' type='submit'>
+          추가
+        </button>
       </Styled.Form>
       {todolist.map((todo) => (
         <TodoItem key={todo.id} todo={todo} />
