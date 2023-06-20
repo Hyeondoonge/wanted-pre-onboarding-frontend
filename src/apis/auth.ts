7aba306d7bf8df232f24d7ad12aec045116a74fe
Hyeondoonge <migu554@naver.com>
2023-06-21 02:22:34 +0900

    :recycle: request 유틸 인증 api 요청 로직에 적용 - #10

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,38 +1,25 @@
 import { SIGNIN_URL, SIGNUP_URL } from 'constants/api';
 import { IUser, Response } from 'interface/common';
+import { request } from 'utils/api';
 
 type SignupBody = IUser;
 
 export async function signUp(signupBody: SignupBody): Response<IUser> {
-  try {
-    const response = await fetch(`${process.env.REACT_APP_API_END_POINT}${SIGNUP_URL}`, {
-      method: 'post',
-      headers: { 'content-type': 'application/json' },
-      body: JSON.stringify(signupBody)
-    });
-
-    if (response.ok) return signupBody;
-    throw new Error(((await response.json()) as { message: string }).message);
-  } catch (error) {
-    if (error instanceof Error) return { message: error.message };
-    return { message: '알 수 없는 에러 입니다' };
-  }
+  return request({
+    path: SIGNUP_URL,
+    body: JSON.stringify(signupBody),
+    headers: { 'content-type': 'application/json' },
+    method: 'post'
+  });
 }
 
 type SigninBody = IUser;
 
 export async function signIn(signinBody: SigninBody): Response<{ access_token: string }> {
-  try {
-    const response = await fetch(`${process.env.REACT_APP_API_END_POINT}${SIGNIN_URL}`, {
-      method: 'post',
-      headers: { 'content-type': 'application/json' },
-      body: JSON.stringify(signinBody)
-    });
-    const result = await response.json();
-    if (response.ok) return result;
-    throw new Error((result as { message: string }).message);
-  } catch (error) {
-    if (error instanceof Error) return { message: error.message };
-    return { message: '알 수 없는 에러 입니다' };
-  }
+  return request({
+    path: SIGNIN_URL,
+    body: JSON.stringify(signinBody),
+    headers: { 'content-type': 'application/json' },
+    method: 'post'
+  });
 }
